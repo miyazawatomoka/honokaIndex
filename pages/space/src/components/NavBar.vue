e70cf80c5a4f54177353c6a795c93fcd4b9e0a44
miyazawatomoka <miyazawa_tomoka@yahoo.co.jp>
2017-02-08 15:31:56 +0800

    fix

--- pages/space/src/components/NavBar.vue
+++ pages/space/src/components/NavBar.vue
@@ -2,13 +2,13 @@
   <div>
     <el-menu default-active="1" class="el-menu-demo" mode="horizontal" @select="handleSelect" :router="true">
       <el-menu-item index="/">主页</el-menu-item>
-      <el-submenu index="recongize">
+      <el-submenu index="/recongize">
         <template slot="title">人物识别</template>
         <el-menu-item index="/recongize/new">新建图片集</el-menu-item>
         <el-menu-item index="/recongize/update">更新图片集</el-menu-item>
         <el-menu-item index="/recongize/uploaded">已上传图片</el-menu-item>
       </el-submenu>
-      <el-menu-item index="setting">设置</el-menu-item>
+      <el-menu-item index="/setting">设置</el-menu-item>
     </el-menu>
   </div>
 </template>
